356aa81d7b462c1383cff4a603f0bc8ee8edf213
Harsh Kumar <harshkumarsingh54321@gmail.com>
2025-03-19 15:50:18 +0530

    get user profile route done and added method to check if token blacklisted or not

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -60,3 +60,10 @@ module.exports.loginUser = async (req, res, next) => {
     res.cookie('token', token);
     res.status(200).json({ token, user });
 }
+
+
+module.exports.getUserProfile = async (req, res, next) => {
+
+    res.status(200).json(req.user);
+
+}
