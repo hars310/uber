d6cfae233240ffac9d9423652c6342ea34fc5913
Harsh Kumar <harshkumarsingh54321@gmail.com>
2025-03-19 18:07:08 +0530

    userlogin frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import StartPage from './pages/StartPage'
+import UserRegister from './pages/UserRegister'
+import { Toaster } from "react-hot-toast";
 const App = () => {
 
   return (
     <div>
+    <Toaster />
       <Routes>
         <Route path='/' element={<StartPage />} />
+        <Route path='/register' element={<UserRegister />} />
       </Routes>
     </div>
   )
