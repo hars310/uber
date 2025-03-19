c872db737c0a194855cc8d9722257e6bc6651807
Harsh Kumar <harshkumarsingh54321@gmail.com>
2025-03-20 01:13:22 +0530

    sample home page showing leaflet map

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  return (
+    <div className="map-container ">
+      <MapContainer 
+        center={[28.6139, 77.2090]} // Default: Delhi, India
+        zoom={13} 
+        scrollWheelZoom={true} 
+        style={{ height: "100vh", width: "100vw" }} // Full-screen map
+      >
+        {/* OpenStreetMap Tile Layer */}
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        
+        {/* Marker Example */}
+        <Marker position={[28.6139, 77.2090]}>
+          <Popup>New Delhi, India</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,34 @@
-import React, { useContext, useEffect } from 'react'
-import { UserDataContext } from '../context/UserContext'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react';
+import { UserDataContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import Map from '../components/Map';
 
 const Home = () => {
-    const { user } = useContext(UserDataContext)
-    const navigate= useNavigate()
-    const token = localStorage.getItem('token')
-    useEffect(()=>{
-        if(!user || !token){
-            navigate('/login')
-        }
-    },[user])
-  return (
-    <div>Home</div>
-  )
-}
+  const { user } = useContext(UserDataContext);
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
 
-export default Home
+  useEffect(() => {
+    if (!user || !token) {
+      navigate('/login');
+    }
+  }, [user]);
+
+  return (
+    <div className="relative w-full h-screen">
+      {/* Full-Screen Map */}
+      <div className="absolute inset-0 z-0">
+        <Map />
+      </div>
+
+      {/* Uber Logo on Top of Map */}
+      <img
+        className="absolute top-5 left-16 w-20 z-[1000]" // Higher z-index
+        src="uber-removebg-preview.png"
+        alt="Uber Logo"
+      />
+    </div>
+  );
+};
+
+export default Home;
