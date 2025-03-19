7e6feed88d0ca918aa66d5bb99365cafa143e889
Harsh Kumar <harshkumarsingh54321@gmail.com>
2025-03-19 15:37:58 +0530

    added morgan package to get details about all api request in terminal

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const userRoutes = require('./routes/user.routes');
 const connectToDb = require('./db/db');
 connectToDb();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan('dev'));
 
 
 
