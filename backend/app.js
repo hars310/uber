fdbac3f897f26420ea99e067b5a3f3f8eb8daa2d
Harsh Kumar <harshkumarsingh54321@gmail.com>
2025-04-07 19:13:35 +0530

    added frontend url in cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,10 @@ const rideRoutes = require('./routes/ride.routes');
 const connectToDb = require('./db/db');
 connectToDb();
 
-app.use(cors());
+const isDev = app.settings.env === 'development'
+const URL = isDev ? "http://localhost:5173" : 'https://uber-ruddy.vercel.app/'
+
+app.use(cors({origin:URL}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
